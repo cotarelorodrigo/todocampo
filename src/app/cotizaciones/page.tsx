0a8c7e49d0de89a9d36d96153652b7e2ce9becb2
Cota <cotarelorodrigo@gmail.com>
2024-02-26 00:20:59 -0300

    add header to cotizaciones and servicios pages

---
+++ src/app/cotizaciones/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Cotizaciones() {
+  return (
+    <div className="flex flex-col w-full h-screen mt-14">
+      <div className="w-full bg-cane-200 h-48 flex flex-col items-center justify-center gap-2">
+        <h2 className="text-4xl font-bold">Cotizaciones</h2>
+        <h3>Ofrecemos las mejores cotizaciones del mercado</h3>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/servicios/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Servicios() {
+  return (
+    <div className="flex flex-col w-full h-screen mt-14">
+      <div className="w-full bg-cane-200 h-48 flex flex-col items-center justify-center gap-2">
+        <h2 className="text-4xl font-bold">Servicios</h2>
+        <h3>Enterate de todo lo que ofrecemos para vos</h3>
+      </div>
+    </div>
+  );
+}
