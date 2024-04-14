6856c07ced44ffdf62a05243ea02f9167c4a6d7c
Cota <cotarelorodrigo@gmail.com>
2024-04-14 04:19:14 -0300

    made nosotros page responsive

--- src/app/nosotros/page.tsx
+++ src/app/nosotros/page.tsx
@@ -4,13 +4,13 @@ import { Separator } from "@/components/ui/separator";
 export default function Nosotros() {
   return (
     <div className="flex flex-col w-full">
-      <div className="w-full bg-cane-200 h-[250px] flex flex-col items-center justify-center gap-2 mt-[70px] bg-[url('/assets/grid.svg')]">
+      <div className="w-full bg-cane-200 h-[250px] flex flex-col items-center justify-center gap-2 mt-[70px] bg-[url('/assets/grid.svg')] p-4">
         <h2 className="text-4xl font-bold">¿Quiénes somos?</h2>
         <h3>Vende tus granos de forma sencilla</h3>
       </div>
-      <div className="w-full px-12 xl:px-64 flex flex-1 flex-col gap-16 pt-10">
+      <div className="container flex flex-1 flex-col gap-16 pt-10 ">
         <div className="flex flex-row gap-8">
-          <div className="w-full xl:w-2/3 h-[750px] relative">
+          <div className="container lg:w-2/3 relative">
             <Image
               src="/assets/tractor.jpeg"
               alt="nosotros"
@@ -28,10 +28,12 @@ export default function Nosotros() {
             />
           </div>
         </div>
-        <div className="flex flex-col items-center xl:items-start">
-          <h1 className="text-4xl font-bold text-cane-600">¿Quiénes somos?</h1>
-          <div className="flex xl:justify-between flex-1 flex-row gap-32 flex-wrap  xl:flex-nowrap justify-center">
-            <div className="w-1/2 flex flex-col justify-evenly gap-4">
+        <div className="flex flex-col xl:items-start container">
+          <h1 className="text-4xl font-bold text-cane-600 hidden lg:block container">
+            ¿Quiénes somos?
+          </h1>
+          <div className="flex lg:justify-between flex-1 flex-row gap-32 flex-wrap lg:flex-nowrap justify-center">
+            <div className="container lg:w-1/2 flex flex-col justify-evenly gap-4">
               <p className="text-lg text-[#797979]">
                 Somos <span className="text-cane-600">GranosDirectos</span>, una
                 plataforma de venta que conecta al productor directamente con
@@ -47,7 +49,7 @@ export default function Nosotros() {
               </p>
               <p className="text-lg text-[#797979]">Todo en el mismo lugar.</p>
             </div>
-            <div className="w-1/2 flex flex-col justify-center gap-8 bg-red-200 container">
+            <div className="container lg:w-1/2 flex flex-col justify-center gap-8">
               <div className="flex flex-row justify-between items-center flex-wrap">
                 <h4 className="text-3xl text-cane-500">Vision</h4>
                 <p className="text-lg text-[#797979]">
