4debd28cb7ac6513a08af5583194ef0da320c642
Cota <cotarelorodrigo@gmail.com>
2024-04-14 03:33:30 -0300

    fix styles

--- src/components/NavBar/SideBard.tsx
+++ src/components/NavBar/SideBard.tsx
@@ -12,12 +12,12 @@ export default function SideBard({
   return (
     <div
       className={cn(
-        "lg:hidden absolute right-0 top-0 flex h-screen w-full flex-col px-4 pt-5 duration-500 z-40 mt-[70px] bg-white border-l border-s-zinc-200",
-        !open ? "translate-x-full" : "transform-none"
+        "lg:hidden absolute right-0 top-0 h-screen w-full px-4 pt-5 duration-500 z-40 mt-[70px] bg-white border-l border-s-zinc-200",
+        !open ? "hidden" : "flex flex-col"
       )}
     >
       <MenuButtons
-        className="lghidden flex flex-col gap-8 items-start"
+        className="lg:hidden flex flex-col gap-8 items-start"
         onClick={onClick}
       />
     </div>
